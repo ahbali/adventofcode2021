38f2d9bb579a7bccd1849e53b1ec88d2248d17df
Ahbali El-Mehdi <el.ahbali@gmail.com>
2021-12-09 10:45:24 +0100

    day06 (part2)

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -11,25 +11,62 @@ fn main() {
         .collect::<Vec<u8>>();
 
     let mut population = 0usize;
+    let mut children_counts: [usize; 7];
 
-    //
-    // part 1
-    //
-    let mut world: Vec<u8> = Vec::with_capacity(1_000_000_000);
-    for element in input {
-        world.push(element);
-        for _ in 0..80 {
-            for i in 0..world.len() {
-                if world[i] == 0 {
-                    world[i] = 6;
-                    world.push(8);
-                } else {
-                    world[i] -= 1;
-                }
-            }
-        }
-        population += world.len();
-        world.clear();
+    // let mut world: Vec<u8> = Vec::with_capacity(10_000_000_000);
+    // world.push(0);
+
+    ////
+    //// part 1
+    ////
+    // children_counts = [0; 7];
+    // const PART1_ITERATIONS: usize = 80;
+
+    // for gen in 1..=PART1_ITERATIONS {
+    //     for i in 0..world.len() {
+    //         if world[i] == 0 {
+    //             world[i] = 6;
+    //             world.push(8);
+    //         } else {
+    //             world[i] -= 1;
+    //         }
+    //     }
+    //     if (PART1_ITERATIONS - gen) < 7 {
+    //         children_counts[PART1_ITERATIONS - gen] = world.len();
+    //     }
+    // }
+    // println!("children_counts = {:?};", children_counts);
+
+    children_counts = [1421, 1401, 1191, 1154, 1034, 950, 905]; // these values were computed using the above commented code.
+    for &element in input.iter() {
+        population += children_counts[element as usize];
     }
     println!("part 1= {}", population);
+    ////
+    //// part 2
+    ////
+    population = 0;
+    // const PART2_ITERATIONS: usize = 256;
+    // for gen in (PART1_ITERATIONS + 1)..=PART2_ITERATIONS {
+    //     for i in 0..world.len() {
+    //         if world[i] == 0 {
+    //             world[i] = 6;
+    //             world.push(8);
+    //         } else {
+    //             world[i] -= 1;
+    //         }
+    //     }
+    //     if (PART2_ITERATIONS - gen) < 7 {
+    //         children_counts[PART2_ITERATIONS - gen] = world.len();
+    //     }
+    // }
+    // println!("children_counts = {:?};", children_counts);
+
+    children_counts = [
+        6703087164, 6206821033, 5617089148, 5217223242, 4726100874, 4368232009, 3989468462,
+    ]; // these values were computed using the above commented code.
+    for element in input {
+        population += children_counts[element as usize];
+    }
+    println!("part 2= {}", population);
 }
